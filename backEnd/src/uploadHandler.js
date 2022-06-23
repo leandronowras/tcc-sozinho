a7229a7d77969e12992d201d66a251ca91f6baa6
Leandro Abreu <leandro@Leandros-Mac-mini-2.local>
2022-06-23 01:08:00 -0300

    add backEnd basic streams

---
+++ backEnd/src/uploadHandler.js
@@ -0,0 +1,58 @@
+import Busboy from "busboy";
+import fs from 'fs'
+import { pipeline } from 'stream/promises'
+import { logger } from "./logger";
+
+export default class UploadHandler {
+    constructor({ io, socketId, downloadsFolder }) {
+      this.io =  io
+      this.socketId =  socketId
+      this.downloadsFolder =  downloadsFolder  
+      this.ON_UPLOAD_EVENT = 'file-upload'
+    }
+
+    // necessario pois o processo eh tao rapido que pode travar o client
+    canExecute(lastExecution) {
+
+    }
+
+    handleFileBytes(filename) {
+      this.lastMessageSent = Date.now()
+
+      // outra maneira de fazer um transform
+      async function* handleData(source) {
+        let processedAlready = 0
+
+        for await (const chunk of source) {
+          yield chunk
+          processedAlready += chunk.length
+          if(!this.canExecute(this.lastMessageSent)) {
+            continue
+          }
+          this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, { processedAlready, filename })
+          logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`)
+        }
+      }
+
+      return handleData.bind(this)
+    }
+
+    async onFile(fieldname, file, filename) {
+      const saveTo = `${this.downloadsFolder}/${filename}`
+      await pipeline(
+        file,
+        this.handleFileBytes.apply(this, [filename]),
+        fs.createWriteStream(saveTo)
+      )
+
+      logger.info(`File [${filename}] finished`)
+    }
+
+    registerEvents(headers, onFinish) {
+      const busboy = new Busboy({ headers })
+      busboy.on("file", this.onFile.bind(this))
+      busboy.on("finish", onFinish)
+
+      return busboy
+    }
+}
---
+++ backEnd/test/_util/testUtil.js
@@ -0,0 +1,37 @@
+import { Readable, Transform, Writable } from 'stream'
+
+export default class TestUtil {
+
+  static generateReadableStream(data) {
+    return new Readable({
+      // objectMode: true,
+      read() {
+        for (const item of data) {
+         this.push(item)
+        }
+
+        this.push(null)
+      }
+    })
+  }
+
+  static generateWritadableStream(onData ) {
+    return new Writable({
+      objectMode: true,
+      write(chunk, enconding, cb) {
+        onData(chunk)
+        cb(null,chunk)
+      }
+    })
+  }
+
+  static generateTransformStream(onData) {
+    return new Transform({
+      objectMode: true,
+      transform(chunk, enconding, cb) {
+         onData(chunk)
+         cb(null, chunk)
+      }
+    })
+  }
+}
