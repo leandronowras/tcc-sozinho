4e39dd230c3607a72e954cddcd7c8134141f0598
Leandro Abreu <leandro@Leandros-Mac-mini-2.local>
2022-06-28 10:40:10 -0300

    certificates working

--- backEnd/apiWeb/src/index.js
+++ backEnd/apiWeb/src/index.js
@@ -1,12 +1,19 @@
-import http from 'http'
+import https from 'https'
+import fs from 'fs'
 import { logger } from './logger.js'
 import { Server } from 'socket.io'
 import Routes from './routes.js'
 
 const PORT = process.env.PORT || 3000
 
+const localHostSSL = {
+  key: fs.readFileSync('/Users/leandro/Documents/ceub/tcc_solo/backEnd/apiWeb/key.pem'),
+  cert: fs.readFileSync('/Users/leandro/Documents/ceub/tcc_solo/backEnd/apiWeb/cert.pem'),
+}
+
 const routes = new Routes()
-const server = http.createServer(
+const server = https.createServer(
+  localHostSSL,
     routes.handler.bind(routes)
 )
 
@@ -23,7 +30,7 @@ io.on("connection", (socket) => logger.info(`someone connected ${socket.id}`))
 
 const startServer = () => {
     const { address, port } = server.address()
-    logger.info(`app running at http://${address}:${port}`)
+    logger.info(`app running at https://${address}:${port}`)
 }
 
 server.listen(PORT, startServer)
